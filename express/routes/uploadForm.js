f65cce6ef813b20be3aa90220c55060d081a5af3
Taotaoop <103357080+Taotaoop@users.noreply.github.com>
2024-01-01 08:48:43 -0600

    Complete  upload
    Working on format

--- express/routes/uploadForm.js
+++ express/routes/uploadForm.js
@@ -2,6 +2,9 @@ var express = require("express");
 var router = express.Router();
 
 router.post("/uploadform", function (req, res,) {
+  console.log("i'm here");
+  console.log(req);
+  console.log("done");
   console.log(req.body);
   console.log(req.params);
   console.log(req.params);

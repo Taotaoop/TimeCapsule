ef3d297574d5ebb8c47cd5d3ada523bdd54b900c
arthur262 <46006708+arthur262@users.noreply.github.com>
2024-01-03 22:04:16 +0800

    1

--- express/express_mysql_db.sql
+++ express/express_mysql_db.sql
@@ -15,14 +15,16 @@ CREATE TABLE `user` (
 
 CREATE TABLE `file` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(20) NOT NULL,
-    `email` VARCHAR(60) NOT NULL,
-    `capsuleText` VARCHAR(2000) NOT NULL,
-    `filepath` VARCHAR(2000) NOT NULL,
+    `name` VARCHAR(20),
+    `time` date NOT NULL,
+    `email` VARCHAR(60),
+    `capsuleText` VARCHAR(2000),
+    `filepath` VARCHAR(2000),
     PRIMARY KEY (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 # 插入三条测试用数据
 INSERT INTO user (`name`, `email`) VALUES ('user1', 'xxx.outlook.com');
-INSERT INTO file (`id`,`name`, `email`,`capsuleText`, `filepath`) VALUES ('0','user2', 'xy.outlook.com','轻微的轻微的','./file/屏幕截图 2023-12-26 121607-1704167982482.png');
+INSERT INTO file (`id`,`name`,`time`, `email`,`capsuleText`, `filepath`) VALUES ('0','user2','1989-11-11', 'xy.outlook.com','轻微的轻微的','./file/屏幕截图 2023-12-26 121607-1704167982482.png');
+INSERT INTO file (`id`,`name`,`time`, `email`,`capsuleText`, `filepath`) VALUES ('0','user2','1999-11-11', 'xy.outlook.com','轻微的轻微的','./file/屏幕截图 2023-12-26 121607-1704167982482.png');
 
